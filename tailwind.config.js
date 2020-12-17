470f44c7eae97bcdfa4113a5b0d9b317db4fd024
cloworm <chloe.echikson@gmail.com>
2020-12-17 18:20:46 -0500

    updated tailwind to purge tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/* eslint-env node */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 module.exports = {
-  purge: [],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
