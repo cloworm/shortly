468bf7ca6d72a1a962a53cad68bde71a813d827d
Chloe Echikson <chloe.echikson@gmail.com>
2020-12-17 18:23:26 -0500

    Setup (#1)
    * added tailwind
    
    * added typescript
    
    * added eslint
    
    * added ci

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { ReactElement } from 'react'
+
+export default function Home(): ReactElement {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
