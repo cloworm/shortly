36422d42ce630bbf49ef9aa0dbe787888605682a
cloworm <chloe.echikson@gmail.com>
2020-12-20 21:24:40 -0500

    fixed indentation

--- components/Boost.tsx
+++ components/Boost.tsx
@@ -7,7 +7,7 @@ const Boost = (): ReactElement => {
     <div className="bg-boost_mobile flex flex-col justify-center lg:bg-boost bg-100-100 lg:bg-auto h-74 lg:h-60 lg:py-10 text-center">
       <div>
         <p className="text-white font-bold text-2xl lg:text-3xl text-center pb-4 lg:pb-6">
-        Boost your links today
+          Boost your links today
         </p>
       </div>
       <div>
